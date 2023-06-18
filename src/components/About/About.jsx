c8d6c7db43eaeaba33ac62a4ff71d2b8dd72429f
musto <christianmustoj@gmail.com>
2023-06-18 18:41:20 -0400

    add about me message

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,10 @@ import './About.css'
 function About() {
   return (
     <>
+    <div className='about-me'>
       <h1>About Me</h1>
+        <p>Welcome! I'm Christian Musto, a certified Software Engineer based in Cleveland, Ohio. With a proven track record in customer service, I seamlessly transitioned into the world of software development, driven by my unwavering passion for creating exceptional user experiences. My unique blend of technical expertise and creative pursuits, including photography, producing music, and creating content for a small community sets me apart from the rest. As you explore my portfolio, you'll discover a versatile professional who brings a fresh perspective and a commitment to excellence. I invite you to connect with me and explore how our collaboration can elevate your software development projects to new heights.</p>
+      </div>
     </>
   )
 }
