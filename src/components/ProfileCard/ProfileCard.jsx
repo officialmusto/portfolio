cee2c19f555d673d924bb72832dc788efbddc51d
musto <christianmustoj@gmail.com>
2023-06-18 19:54:07 -0400

    update profile-card to center on site

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -7,13 +7,19 @@ import profilePicture from '../../assets/profile-picture.jpg'
 function ProfileCard() {
   return (
     <>
+      <section id="home">
       <div className='profile-card'>
-        <img 
-        className="profile-picture"
-        src={profilePicture} 
-        alt="profile picture" />
-
+          <img 
+          className="profile-picture"
+          src={profilePicture} 
+          alt="profile picture" />
+          <div className='links'>
+          <h3>Email: christianmustoj@gmail.com</h3>
+          <h3>linkedIn: christianmustoj@gmail.com</h3>
+          <h3>GitHub: christianmustoj@gmail.com</h3>
+          </div>
       </div>
+      </section>
     </>
   )
 }
