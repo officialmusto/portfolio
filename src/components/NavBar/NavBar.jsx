072e15aeb559793f4033ed981a5a9ee3175e64d5
musto <christianmustoj@gmail.com>
2023-06-18 21:09:11 -0400

    update links to include proper hrefs to proper locations

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,30 @@
 // npm modules
-import { HashLink } from 'react-router-hash-link'
+import { HashLink } from "react-router-hash-link"
 
 // css
-import '../NavBar/NavBar.css'
+import "../NavBar/NavBar.css"
 
 function NavBar() {
   return (
     <div id="nav-el">
-      <nav className='nav'>
-      <HashLink smooth to="/#home">
+      <nav className="nav">
+        <HashLink smooth to="/#home">
           home
-      </HashLink>
+        </HashLink>
 
-      <HashLink smooth to="/#about">
-        about
-      </HashLink>
+        <HashLink smooth to="/#about">
+          about
+        </HashLink>
 
-      <HashLink smooth to="/#portfolio">
-        portfolio
-      </HashLink>
+        <HashLink smooth to="/#portfolio">
+          portfolio
+        </HashLink>
 
-      <HashLink smooth to="/#resume">
-        resume
-      </HashLink>
-  </nav>
-</div>
+        <HashLink smooth to="/#resume">
+          resume
+        </HashLink>
+      </nav>
+    </div>
   )
 }
 
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,24 +1,49 @@
 // css
-import '../ProfileCard/ProfileCard.css'
+import "../ProfileCard/ProfileCard.css"
 
 // assets
-import profilePicture from '../../assets/profile-picture.jpg'
+import profilePicture from "../../assets/profile-picture.jpg"
+import githubLogo from "../../assets/github-logo.svg"
+import emailLogo from "../../assets/email-logo.svg"
+import linkedinLogo from "../../assets/linkedin-logo.svg"
 
 function ProfileCard() {
   return (
     <>
       <section id="home">
-      <div className='profile-card'>
-          <img 
-          className="profile-picture"
-          src={profilePicture} 
-          alt="profile picture" />
-          <div className='links'>
-            <h3>Email: christianmustoj@gmail.com</h3>
-            <h3>linkedIn: christianmustoj@gmail.com</h3>
-            <h3>GitHub: christianmustoj@gmail.com</h3>
+        <div className="profile-card">
+          <img
+            className="profile-picture"
+            src={profilePicture}
+            alt="profile picture"
+          />
+          <div className="links">
+            <div id="email-link">
+              <img
+                src={emailLogo}
+                alt="email logo"
+                style={{ height: "23px" }}
+              />
+              <a href="mailTo: christianmustoj@gmail.com">christianmustoj@gmail.com</a>
+            </div>
+            <div id="github-link">
+              <img
+                src={githubLogo}
+                alt="github logo"
+                style={{ height: "20px" }}
+              />
+              <a href="https://github.com/officialmusto?tab=repositories">GitHub</a>
+            </div>
+            <div id="linkedin-link">
+              <img
+                src={linkedinLogo}
+                alt="email logo"
+                style={{ height: "20px" }}
+              />
+              <a href="https://www.linkedin.com/in/christian-musto/">LinkedIn</a>
+            </div>
           </div>
-      </div>
+        </div>
       </section>
     </>
   )
