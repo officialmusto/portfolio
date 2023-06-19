072a0ac54290acd96cf10440557008d9e22d20d0
musto <christianmustoj@gmail.com>
2023-06-19 12:37:55 -0400

    update to add all return information to projectcard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,12 +1,21 @@
 // css
 import '../ProjectCard/ProjectCard.css'
 
-function ProjectCard() {
+
+
+function ProjectCard( { project } ) {
   return (
     <>
-    
+      <h2>{project.title}</h2>
+      <img src={project.thumbnail} alt="thumbnail" />
+      <h2>{project.desc}</h2>
+      {project.badges.map((badge, idx) => {
+        return <img key={idx} src={badge} alt="badge icon" />
+      })}
     </>
   )
 }
 
 export default ProjectCard
+
+// ADD LINKS FOR GITHUB // DEPLOYMENT SITE
--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -10,26 +10,36 @@ import reactIcon from "../../../public/assets/icons/react-icon.svg"
 import sequelizeIcon from "../../../public/assets/icons/sequelize-icon.svg"
 import tsIcon from "../../../public/assets/icons/typescript-icon.svg"
 import viteIcon from "../../../public/assets/icons/vite-icon.svg"
+import bjThumbnail from "../../../public/assets/blackjack-screenshot.png"
+import sbThumbnail from "../../../public/assets/smash-reviews-screenshot.png"
+import ssThumbnail from "../../../public/assets/styleswipe-screenshot.png"
+import hhThumbnail from "../../../public/assets/habit-hero-screenshot.png"
 
+// components
+import ProjectCard from "../ProjectCard/ProjectCard"
 
 function ProjectList() {
   const projects = [
     {title: "BlackJack21",
+    thumbnail: bjThumbnail,
     desc: "description of blackjack project.",
     badges: [
       jsIcon, htmlIcon, cssIcon, gitIcon 
     ]},
     {title: "Smash Bros. Reviews",
+    thumbnail: sbThumbnail,
     desc: "description of Smash Bros. Reviews project.",
     badges: [
       jsIcon, htmlIcon, cssIcon, gitIcon, mongoDbIcon, nodejsIcon
     ]},
     {title: "StyleSwipe",
+    thumbnail: ssThumbnail,
     desc: "description of StyleSwipe project.",
     badges: [
       jsIcon, htmlIcon, cssIcon, gitIcon, mongoDbIcon, nodejsIcon, reactIcon, viteIcon
     ]},
     {title: "Habit Hero!",
+    thumbnail: hhThumbnail,
     desc: "description of Habit Hero! project.",
     badges: [
       jsIcon, htmlIcon, cssIcon, gitIcon, mongoDbIcon, nodejsIcon, reactIcon, viteIcon, tsIcon, postgresIcon, sequelizeIcon
@@ -40,36 +50,11 @@ function ProjectList() {
   return (
     <>
       <h1>Project List</h1> 
-      {projects.map(project => {
-        return <>
-        <h2>{project.title}</h2>
-        <h2>{project.desc}</h2>
-        {project.badges.map((badge, idx) => {
-          return <img key={idx} src={badge} alt="badge icon" />
-        })}</>
+      {projects.map((project, idx) => {
+        return <ProjectCard key={idx} project={project} />
       })}
     </>
   )
 }
 
 export default ProjectList
-
-
-
-/* each project card will have
-1. title
-2. img
-3. desc.
-4. links-to github repo & deployed site
-5. languages-used badges
-
--create an array of objects
--each object will have those 5 properties
-
--map through array of badges in langUsedBadges // or add badges manually to project
-
--map through the array
-
-drill project card props if necessary 
-
-*/
